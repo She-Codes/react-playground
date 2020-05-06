1dce54c5152487c5fc18c546608935983438c800
She-Codes <n.y.shaw@gmail.com>
2020-05-05 21:34:17 -0400

    added react router example

--- react-router-example/src/App.js
+++ react-router-example/src/App.js
@@ -4,17 +4,45 @@ import {
   Switch,
   Route,
   Link,
-  useRouteMatch,
-  useParams,
+  // useRouteMatch,
+  // useParams,
 } from 'react-router-dom';
 import './App.css';
+
+const Home = () => <h2>Home</h2>;
+
+const About = () => <h2>About</h2>;
+
+const User = () => <h2>User</h2>;
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
-        <ul></ul>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/user">User</Link>
+          </li>
+        </ul>
       </div>
+
+      <Switch>
+        <Route path="/user">
+          <User />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 };
